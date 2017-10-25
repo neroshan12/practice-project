e97b6956b77d7a0c86689c05af8fb1f02b7b4c67
Luan van Pletsen <luanvanpletsen@msn.com>
2017-10-25 13:18:23 +0100

    loop over locations object

--- app.js
+++ app.js
@@ -1,51 +1,37 @@
-// console.log('test');
-// var config = require('/config.json')
-// console.log(config);
-
-
-
+var locations = {
+  'hcf': {
+  'coords':"51.5316758,-0.0690335",
+  'divElement': 'hcf-distance'
+  },
+  'lbc': {
+    'coords':"51.5280202,-0.0534266",
+    'divElement': 'lbc-distance'
+  },
+  'lsc': {
+    'coords':"51.5175798,-0.0802112",
+    'divElement': 'lsc-distance',
+  },
+  'gc': {
+    'coords':"51.5210263,-0.0838455",
+    'divElement': 'gc-distance',
+  }
+};
+var locationNames = ['hcf','lbc','lsc','gc']
 var currentLocation;
-// var googleResponse;
-// var googleJSON;
+
 navigator.geolocation.getCurrentPosition(function(position) {
   currentLocation = (`${position.coords.latitude}, ${position.coords.longitude}`);
-  var hcf = "51.5316758,-0.0690335";
-  var lbc = "51.5280202,-0.0534266";
-  var lsc = "51.5175798,-0.0802112";
-  var gc = "51.5210263,-0.0838455";
-  var locations = {
-    'hcf': {
-    'coords':"51.5316758,-0.0690335",
-    'divElement': 'hcf-distance'
-    },
-    'lbc': {
-      'coords':"51.5280202,-0.0534266",
-      'divElement': 'lbc-distance'
-    },
-    'lsc': {
-      'coords':"51.5175798,-0.0802112",
-      'divElement': 'lsc-distance',
-    },
-    'gc': {
-      'coords':"51.5210263,-0.0838455",
-      'divElement': 'gc-distance',
+  for (var location in locations) {
+    if (locations.hasOwnProperty(location)){
+      writeDistanceToScreen(locations[location]['coords'], locations[location]['divElement'])
     }
-  };
-
-  writeDistanceToScreen(locations['hcf']['coords'], locations['hcf']['divElement'])
-  writeDistanceToScreen(locations['lbc']['coords'], locations['lbc']['divElement'])
-  writeDistanceToScreen(locations['lsc']['coords'], locations['lsc']['divElement'])
-  writeDistanceToScreen(locations['gc']['coords'], locations['gc']['divElement'])
-
+  }
 });
 
-// console.log(currentLocation)
-// console.log('Hello')
 function writeDistanceToScreen(destinationCoordinates, elementID) {
   var dist = callGoogleForDistance(currentLocation, destinationCoordinates)
   displayDistance(dist, elementID)
 }
-
 
 function callGoogleForDistance(location, destination){
   var xhttp = new XMLHttpRequest();
@@ -54,13 +40,8 @@ function callGoogleForDistance(location, destination){
   googleResponse = xhttp.response;
   googleJSON = JSON.parse(googleResponse);
   return googleJSON.rows[0].elements[0].distance['value'];
-  // displayDistanceFromJSON(googleJSON);
 }
 
 function displayDistance(distance, elementID) {
   document.getElementById(elementID).innerHTML = distance + 'm';
 }
-
-// function displayDistanceFromJSON(JSONresponse) {
-//   document.getElementById("hcf-distance").innerHTML = JSONresponse.rows[0].elements[0].distance['value'] + 'm';
-// }
